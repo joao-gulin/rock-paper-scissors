507341818f8a3b266450a6216547cd6cab17ed21
joao-gulin <joaogulin@gmail.com>
2023-11-26 22:54:51 +0000

    .

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const computerChoices = ['Rock', 'Paper', 'Scissor']
+
+function getComputerChoice(arr) {
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    //get random item
+    const item = arr[randomIndex];
+
+    return item
+}
